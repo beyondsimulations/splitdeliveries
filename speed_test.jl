42d86928f77c6e837af2f10581d1c3f7a6b762a7
Vlcek Server <atlantic@posteo.de>
2022-06-01 12:39:42 +0200

    benchmark_3

--- speed_test.jl
+++ speed_test.jl
@@ -1,26 +1,27 @@
-# import timer
 using TimerOutputs
-tmr = TimerOutput()
+begin 
+    tmr = TimerOutput()
 
-## import packages
-include("load_packages.jl")
+    ## import packages
+    include("load_packages.jl")
 
-# state the capacites
-capacity = [8000,7000,6000]
+    # state the capacites
+    capacity = [10000,10000,10000]
 
-## generate transactions
-print("\nstarting generation of transactions.")
-trans = RANDOMTRANS(20000,1000000,1000,0.00,0.01,0.00,0.50,0.10,0.10)
+    ## generate transactions
+    print("\nstarting generation of transactions.")
+    trans = RANDOMTRANS(30000,1000000,1000,0.00,0.01,0.00,0.50,0.10,0.10)
 
-## generate the coappearance matrix
-@time @timeit tmr "coappearance matrix" Q = COAPPEARENCE(trans)
+    ## generate the coappearance matrix
+    @time @timeit tmr "coappearance matrix" Q = COAPPEARENCE(trans)
 
-## call the heuristic
-print("\nstarting the heuristic")
-@time @timeit tmr "chisquare heuristic" W = CHISQUAREHEUR(trans, capacity, Q, 0.01, true)
-print("\nheuristic finished")
+    ## call the heuristic
+    print("\nstarting the heuristic")
+    @time @timeit tmr "chisquare heuristic" W = CHISQUAREHEUR(trans, capacity, Q, 0.01, true)
+    print("\nheuristic finished")
 
-## benchmark the parcels sent out
-combination = COMBINEWAREHOUSES(capacity)
-parcels = PARCELSSEND(trans, W, capacity, combination)
-show(tmr)
+    ## benchmark the parcels sent out
+    combination = COMBINEWAREHOUSES(capacity)
+    parcels = PARCELSSEND(trans, W, capacity, combination)
+    show(tmr)
+end
