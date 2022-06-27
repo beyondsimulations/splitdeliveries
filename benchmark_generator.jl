ca0961b3be4814abc6f92421fa0a4ef6305dd61b
Vlcek Server <atlantic@posteo.de>
2022-06-27 10:05:51 +0200

    prepared for benchmark

--- benchmark_generator.jl
+++ benchmark_generator.jl
@@ -1,8 +1,8 @@
 ## import packages
 include("load_packages.jl")
 
-experiment = "1_200skus"
-capa = 200:200:200
+experiment = "b4_10000skus"
+capa = 10000:10000:80000
 ware = 2:1:4
 diff = 0.0:0.10:0.20
 buff = 0.0:0.10:0.20
