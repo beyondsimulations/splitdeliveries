3fbc783ea67948e73e82f1da7664d9a1e981f03d
Tobias Vlcek <tobias.vlcek@uni-hamburg.de>
2022-06-27 10:19:44 +0200

    aggregation

--- aggregate_benchmarks.jl
+++ aggregate_benchmarks.jl
@@ -5,44 +5,55 @@ using StatsPlots
 using Statistics
 using LaTeXStrings
 
-experiments = ["2_100skus","3_1000skus","4_10000skus"]
-dependencies = ["IND","MD","HD"]
+experiments     = ["2_100skus","3_1000skus","4_10000skus"]
+dependencies    = ["IND","MD","HD"]
+datasets        = ["a_parcels_sent","b_duration"]
 
-function load_data(experiment, dependencies)
-    parcels = DataFrame[]
-    duration = DataFrame[]
-    for dependency in dependencies
-        loadframe_parcels = CSV.read("results/$(experiment)_a_parcels_sent_$dependency.csv", DataFrame)
-        loadframe_duration = CSV.read("results/$(experiment)_b_duration_$dependency.csv", DataFrame)
-        insertcols!(loadframe_parcels, 1, :dependency .=> "$dependency")
-        insertcols!(loadframe_duration, 1, :dependency .=> "$dependency")
-        loadframe_duration = select!(loadframe_duration, Not(:RND))
-        isempty(parcels) ? parcels = loadframe_parcels : parcels = append!(parcels,loadframe_parcels)
-        isempty(duration) ? duration = loadframe_duration : duration = append!(duration,loadframe_duration)
-    end
-    insertcols!(parcels, 4, :capacity_base .=> 0)
-    insertcols!(parcels, 6, :buffer_rel .=> 0.0)
-    insertcols!(duration, 4, :capacity_base .=> 0)
-    insertcols!(duration, 6, :buffer_rel .=> 0.0)
-    parcels[:,:capacity_base] = parcels[:,:capacity] - parcels[:,:buffer]
-    duration[:,:capacity_base] = duration[:,:capacity] - duration[:,:buffer]
-    for row = 1:nrow(parcels)
-        parcels[row,:buffer] == 0.0 ? nothing : parcels[row,:buffer_rel] = parcels[row,:buffer]/parcels[row,:capacity_base]
-        duration[row,:buffer] == 0.0 ? nothing : duration[row,:buffer_rel] = duration[row,:buffer]/duration[row,:capacity_base]
-    end
-    allowmissing!(parcels)
-    allowmissing!(duration)
-    for i = 7:ncol(duration)
-        for j = 1:nrow(duration)
-            duration[j,i] == 0.0 ? duration[j,i] = missing : nothing
-            parcels[j,i] == 0.0 ? parcels[j,i] = missing : nothing
+function load_data()
+    frame = DataFrame[]
+    for experiment in experiments
+        for dataset in datasets
+            for dependency in dependencies
+                loadframe = CSV.read("results/$(experiment)_$(dataset)_$dependency.csv", DataFrame)
+                loadcapacity = CSV.read("results/$(experiment)_$(dataset)_$dependency.csv", DataFrame)
+                insertcols!(loadframe, 1, :experiment .=> "$(experiment)")
+                insertcols!(loadframe, 2, :dataset .=> "$(dataset)")
+                insertcols!(loadframe, 3, :dependency .=> "$dependency")
+                insertcols!(loadframe, 6, :capacity_base .=> 0)
+                insertcols!(loadframe, 7, :capvariation .=> 0)
+                insertcols!(loadframe, 9, :buffer_rel .=> 0.0)
+                loadframe[:,:capacity_base] = loadframe[:,:capacity] - loadframe[:,:buffer]
+                for row = 1:nrow(loadframe)
+                    loadframe[row,:buffer] == 0.0 ? nothing : loadframe[row,:buffer_rel] = loadframe[row,:buffer]/loadframe[row,:capacity_base]
+                end
+                allowmissing!(loadframe)
+                base  = loadframe[1,:wareh]
+                base_ver = 0
+                for i = 1:nrow(loadframe)
+                    if loadframe[i,:wareh] == base
+                        base_ver += 1
+                        loadframe[i,:capvariation] = base_ver
+                    else
+                        base_ver = 1
+                        base = loadframe[i,:wareh]
+                        loadframe[i,:capvariation] = base_ver
+                    end
+                end
+                for i = 10:ncol(loadframe)
+                    for j = 1:nrow(loadframe)
+                        loadframe[j,i] == 0.0 ? loadframe[j,i] = missing : nothing
+                    end
+                end
+                loadframe = stack(loadframe,10:ncol(loadframe))
+                isempty(frame) ? frame = loadframe : frame = append!(frame,loadframe)
+            end
         end
     end
-    parcels = stack(parcels,7:ncol(parcels))
-    duration = stack(duration,7:ncol(duration))
-    return parcels, duration
+    return frame
 end
 
+frame = load_data()
+CSV.write("results/aggregated.csv",frame)
 
 for experiment in experiments
     parcels, duration = load_data(experiment, dependencies)
