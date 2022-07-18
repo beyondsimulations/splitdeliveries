6d5a12e5cc523ad2c76a9d1bc57cf40daef18477
Tobias Vlcek <beyond@posteo.de>
2022-07-18 06:28:44 +0100

    benchmark sig level

--- main_benchmark_settings.jl
+++ main_benchmark_settings.jl
@@ -12,12 +12,12 @@
 ### ID
 ### MD
 ### HD
-    experiment = "b4_10000skus"
+    experiment = "b1_40skus"
     dependency = "MD"
 
 #  Specify the number of orders and the ratio between test
 ## and training data for the generated transactional data sets
-    orders     = 2000000
+    orders     = 20000
     train_test = 0.50
 
 # load the data that specifies the dependencies
@@ -28,17 +28,17 @@
     ren_lock = ReentrantLock()
 
 # Choose Optimisations and Heuristics to evaluate in the benchmark
-    start = DataFrame(QMKO  = [0], # quadratic-multiple knapsack heuristic with CPLEX as solver
-                      QMK   = [0], # quadratic-multiple knapsack heuristic with SBB as solver
+    start = DataFrame(QMKO  = [1], # quadratic-multiple knapsack heuristic with CPLEX as solver
+                      QMK   = [1], # quadratic-multiple knapsack heuristic with SBB as solver
                       CHIM  = [1], # main chi-square heuristic without local search
                       CHI   = [1], # chi-square heuristic + local search based on the QMK objective function
-                      KL    = [0], # K-LINK heuristic by Zhang, W.-H. Lin, M. Huang and X. Hu (2021) https://doi.org/10.1016/j.ejor.2019.07.004
-                      KLQ   = [0], # K-LINK optimisation with SBB by Zhang, W.-H. Lin, M. Huang and X. Hu (2021) https://doi.org/10.1016/j.ejor.2019.07.004
+                      KL    = [1], # K-LINK heuristic by Zhang, W.-H. Lin, M. Huang and X. Hu (2021) https://doi.org/10.1016/j.ejor.2019.07.004
+                      KLQ   = [1], # K-LINK optimisation with SBB by Zhang, W.-H. Lin, M. Huang and X. Hu (2021) https://doi.org/10.1016/j.ejor.2019.07.004
                       GO    = [1], # greedy orders heuristic by A. Catalan and M. Fisher (2012) https://doi.org/10.2139/ssrn.2166687
-                      GP    = [0], # greedy pairs heuristic by A. Catalan and M. Fisher (2012) https://doi.org/10.2139/ssrn.2166687
+                      GP    = [1], # greedy pairs heuristic by A. Catalan and M. Fisher (2012) https://doi.org/10.2139/ssrn.2166687
                       GS    = [1], # greedy seeds heuristic by A. Catalan and M. Fisher (2012) https://doi.org/10.2139/ssrn.2166687
                       BS    = [1], # bestselling heuristic by A. Catalan and M. Fisher (2012) https://doi.org/10.2139/ssrn.2166687
-                      OPT   = [0], # optimisation model to determine the optimal solution with CPLEX
+                      OPT   = [1], # optimisation model to determine the optimal solution with CPLEX
                       RND   = [1]) # random allocation of SKUs (cannot be deactivated)
 
 # Parameters for the KLINK heuristic
@@ -66,7 +66,7 @@
 
 # Parameters for CHISQUARE
 ## sig: significance level alpha for the chi-square tests
-    sig = 0.01
+    sig = 0.001
 
 # Parameters for RANDOM
 ## iterations: number of different random allocations for the comparison
